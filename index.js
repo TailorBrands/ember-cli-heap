076cdd4c2ce76e7244aba54086220e59cb9d8f24
Tal Tchernihovski <taltcher@gmail.com>
2022-11-29 18:33:36 +0200

    Modify script injection location to head

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: require('./package').name,
 
   contentFor: function(type, config) {
-    if (type === 'head-footer' && config.heap && !config.heap.development && config.heap.projectId) {
+    if (type === 'head' && config.heap && !config.heap.development && config.heap.projectId) {
       return '<script type="text/javascript">window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};heap.load("' + config.heap.projectId + '");</script>';
     }
   }
